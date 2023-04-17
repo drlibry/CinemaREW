2a597de7aaa18c28fcd9d6257fe7c2d9e0c698ae
drlibry <dasha.rybakova.03@gmail.com>
2023-04-17 19:54:41 +0300

    Добавлена документация

---
+++ src/main/java/com/example/CinemaREW/CinemaRewApplication.java
@@ -0,0 +1,13 @@
+package com.example.CinemaREW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaRewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaRewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CinemaREW/CinemaRewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CinemaREW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaRewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
