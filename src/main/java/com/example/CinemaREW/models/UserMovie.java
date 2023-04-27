54788698ed2315a2ebd90a7eeb5727684487c956
alina <alirusbay@gmail.com>
2023-04-28 00:25:07 +0300

    Добавлены User и UserMovie с заготовкой для БД

---
+++ src/main/java/com/example/CinemaREW/models/UserMovie.java
@@ -0,0 +1,32 @@
+package com.example.CinemaREW.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+
+@Entity
+@Table(name="user_movie")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserMovie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private User user;
+
+    @NonNull
+    @OneToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private Movie movie;
+
+    @NonNull
+    private String status = "not added";
+
+}
